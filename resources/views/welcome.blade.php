8880e7cbc0c8331f7f5f6da3c584e8e578194b76
TheOliver413 <oliverchemas@hotmail.com>
2023-10-03 07:56:36 -0500

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.base')
+
+@section('content')
+    <div class="row">
+        <div class="alert alert-success mt-2">
+            <div>
+                <h2>Control de tareas</h2>
+            </div>
+            <div>
+                <a href="/tasks" class="btn btn-success">Ingresar</a>
+            </div>
+        </div>
+    </div>
+@endsection
